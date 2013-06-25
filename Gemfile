a181f51b8e981bc0afab15eaffddeeb9bb23fa6a
Neal Barbaro <nealbarbaro@gmail.com>
2013-06-24 18:51:09 -0700

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'pry-debugger'
 gem 'better_errors'
 gem 'binding_of_caller'
 gem 'meta_request'
+gem 'bootstrap-sass'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 DreamJournal::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
 
-  get "static_pages/about"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
+  match '/signup', to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +58,7 @@ DreamJournal::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
