0d3618bcbb232532d749366ba42a1edb91371627
Neal Barbaro <nealbarbaro@gmail.com>
2013-07-22 09:44:17 -0700

    refactored js keyDisable

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,69 +1,15 @@
 $(document).ready(function(){
 
   var nightTimer;
+  var keyArray = [9, 17, 18, 19, 20, 33, 34, 35, 36, 37, 38, 39, 40, 45, 46,
+   91, 92, 93, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 109,
+   110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 144,
+   145, 186, 187, 189, 191, 192, 219, 220, 221, 222, 91, 92, 93];
 
   var keyDisable = function(){
     $(document).on("keydown.nightMode", function(e){
-      switch(e.which){
-        case 9:     e.preventDefault();  break;
-        case 17:    e.preventDefault();  break;
-        case 18:    e.preventDefault();  break;
-        case 19:    e.preventDefault();  break;
-        case 20:    e.preventDefault();  break;
-        case 33:    e.preventDefault();  break;
-        case 34:    e.preventDefault();  break;
-        case 35:    e.preventDefault();  break;
-        case 36:    e.preventDefault();  break;
-        case 37:    e.preventDefault();  break;
-        case 38:    e.preventDefault();  break;
-        case 39:    e.preventDefault();  break;
-        case 40:    e.preventDefault();  break;
-        case 45:    e.preventDefault();  break;
-        case 46:    e.preventDefault();  break;
-        case 91:    e.preventDefault();  break;
-        case 92:    e.preventDefault();  break;
-        case 93:    e.preventDefault();  break;
-        case 96:    e.preventDefault();  break;
-        case 97:    e.preventDefault();  break;
-        case 98:    e.preventDefault();  break;
-        case 99:    e.preventDefault();  break;
-        case 100:   e.preventDefault();  break;
-        case 101:   e.preventDefault();  break;
-        case 102:   e.preventDefault();  break;
-        case 103:   e.preventDefault();  break;
-        case 104:   e.preventDefault();  break;
-        case 105:   e.preventDefault();  break;
-        case 106:   e.preventDefault();  break;
-        case 107:   e.preventDefault();  break;
-        case 109:   e.preventDefault();  break;
-        case 110:   e.preventDefault();  break;
-        case 111:   e.preventDefault();  break;
-        case 112:   e.preventDefault();  break;
-        case 113:   e.preventDefault();  break;
-        case 114:   e.preventDefault();  break;
-        case 115:   e.preventDefault();  break;
-        case 116:   e.preventDefault();  break;
-        case 117:   e.preventDefault();  break;
-        case 118:   e.preventDefault();  break;
-        case 119:   e.preventDefault();  break;
-        case 120:   e.preventDefault();  break;
-        case 121:   e.preventDefault();  break;
-        case 122:   e.preventDefault();  break;
-        case 123:   e.preventDefault();  break;
-        case 144:   e.preventDefault();  break;
-        case 145:   e.preventDefault();  break;
-        case 186:   e.preventDefault();  break;
-        case 187:   e.preventDefault();  break;
-        case 189:   e.preventDefault();  break;
-        case 191:   e.preventDefault();  break;
-        case 192:   e.preventDefault();  break;
-        case 219:   e.preventDefault();  break;
-        case 220:   e.preventDefault();  break;
-        case 221:   e.preventDefault();  break;
-        case 222:   e.preventDefault();  break;
-        case 91:    e.preventDefault();  break;
-        case 92:    e.preventDefault();  break;
-        case 93:    e.preventDefault();  break;
+      if (_.contains(keyArray, e.which)){
+        e.preventDefault();
       }
     });
   }; // keyDisable
